8cad7fc8057f22bcd422a7b6e0a6198f3829fe54
Mathew Oakes <support@mathewoak.es>
2017-01-26 22:01:59 +1100

    Correct resolution of window to its screen, with tests

--- get_window_and_resize.py
+++ get_window_and_resize.py
@@ -26,24 +26,6 @@ def frame_to_bounds(frame):
         frame.origin.y + frame.size.height,
     ]
 
-def get_windows_screen(bounds):
-    centre = (
-        bounds[0] + (bounds[2] - bounds[0]) / 2,
-        bounds[1] + (bounds[3] - bounds[1]) / 2
-    )
-    print('centre', centre)
-    screens = get_screens()
-    screenlist = []
-    for screen in screens:
-        tf = frame_to_bounds(screen.frame())
-        #if screenlist[len(screenlist)-1]:
-            
-        screenlist.append(tf)
-        if tf[0] <= centre[0] <= tf[2] and \
-           tf[1] <= centre[1] <= tf[3]:
-            return screen
-    print('sl', screenlist)
-
 def screenlist_to_desktop(screenlist):
     if len(screenlist) == 1:
         return screenlist[0]
@@ -81,38 +63,23 @@ def coordinate_screens(screenlist):
         sh = s[3] - s[1]
         sw = s[2] - s[0]
         if s[1] < 0 and s[3] == 0:
-            # print('zeroscreen is above')
+            #print('zeroscreen is above')
             s[1] = zeroscreen[3]
             s[3] = s[1] + sh
         elif s[1] == zeroscreen[3]:
-            # print('zeroscreen is below')
-            zeroscreen[1] = s[1]
-            zeroscreen[3] = zeroscreen[3] + sh
-            s[1] = 0.0
-            s[3] = sh
+            #print('zeroscreen is below')
+            s[1] = - sh
+            s[3] = 0
         elif s[0] < 0 and s[3] == zeroscreen[3]:
             #print('zeroscreen is right')
-            zeroscreen[0] += sw
-            zeroscreen[2] += sw
-            s[0] += sw
-            s[2] += sw
             s[3] += abs(s[1])
             s[1] += abs(s[1])
         elif s[0] == zeroscreen[2]:
-            # print('zeroscreen is left')
+            #print('zeroscreen is left')
             s[1] += abs(s[1])
     screenlist.insert(0, zeroscreen)
     return screenlist
 
-"""
-
-Screen origins are measured from the top left corner of the zero screen in positive down and right.
-
-Window origins are measured from bottom left corner of the zero screen
-in positive right and down.
-
-"""    
-    
 def which_screen_contains_window(screenlist, window_bounds):
     winsize = window_bounds[2] - window_bounds[0], window_bounds[3] - window_bounds[1]
     winpos = window_bounds[0], window_bounds[1]
@@ -120,7 +87,6 @@ def which_screen_contains_window(screenlist, window_bounds):
     centery = winpos[1] + winsize[1] / 2.0
     screencount = 0
     for screen in screenlist:
-        print('sss', screen, centerx, centery)
         if screen[0] < centerx < screen[2] and \
            screen[1] < centery < screen[3]:
             break
@@ -130,32 +96,11 @@ def which_screen_contains_window(screenlist, window_bounds):
 def doit(direction=-1, resize=1):
     thwin = get_active_window()
     bounds = thwin.bounds()
-    """
-    winsize = bounds[2] - bounds[0], bounds[3] - bounds[1]
-    winpos = bounds[0], bounds[1]
-    centerx = winpos[0] + winsize[0] / 2.0
-    centery = winpos[1] + winsize[1] / 2.0
-    sorigin = winpos #s.frame().origin
-    ssize = winsize #s.frame().size
-    """
     screens = NSScreen.screens()
-    screenlist = [frame_to_bounds(s.frame()) for s in screens]
-    print('scr', screenlist)
-    print('bou', bounds)
+    screenlist = coordinate_screens([frame_to_bounds(s.frame()) for s in screens])
     screenid = which_screen_contains_window(screenlist, bounds)
-    print('window on screen', screenid)
-    """     for s in 
-        fbounds = 
-        print(s, fbounds, NSScreen._isZeroScreen(s), NSScreen._menuBarHeight(s))
-        #s.convertRectFromBacking_(
+    print('window on screen', screens[screenid].frame())
 
-Screens must be aligned with each other.
-
-
-
-
-
-"""
     """
     print('sss', sorigin, ssize)
     for t in dir(NSScreen):
--- test_windowing.py
+++ test_windowing.py
@@ -3,7 +3,6 @@ import pytest
 from get_window_and_resize import (
     get_active_window,
     get_screens,
-    get_windows_screen,
     frame_to_bounds,
     which_screen_contains_window,
     screenlist_to_desktop,
@@ -55,16 +54,16 @@ def test_coordinate_screens():
     assert csl == [[0.0, 0.0, 1440.0, 900.0], [0.0, 900.0, 1920.0, 2100.0]], \
         'screens not coordinated when zeroscreen is above'
     csl = coordinate_screens(screen_zero_below())
-    assert csl == [[0.0, 900.0, 1440.0, 2100.0], [0.0, 0.0, 1920.0, 1200.0]], \
+    assert csl == [[0.0, 0.0, 1440.0, 900.0], [0.0, -1200.0, 1920.0, 0.0]], \
         'screens not coordinated when zeroscreen is below'
     csl = coordinate_screens(screen_zero_right())
-    assert csl == [[1920.0, 0.0, 3360.0, 900.0], [0.0, 0.0, 1920.0, 1200.0]], \
+    assert csl == [[0.0, 0.0, 1440.0, 900.0], [-1920.0, 0.0, 0.0, 1200.0]], \
         'screens not coordinated when zeroscreen is right'
     csl = coordinate_screens(screen_zero_left())
-    assert csl == [[0.0, 0.0, 1440.0, 900.0], [1440.0, 0.0, 3360.0, 1200.0]], \
+    assert csl == [[0.0, 0.0, 1440.0, 900.0], [1440.0, 0.0, 3360.0, 900.0]], \
         'screens not coordinated when zeroscreen is left'
 
-""" 
+"""
 zeroscreen     screens()                                           window bounds screen 2
 above [[0.0, 0.0, 1440.0, 900.0], [0.0, -1200.0, 1920.0, 0.0]]     (523, 1499, 1116, 2038)
 below [[0.0, 0.0, 1440.0, 900.0], [0.0, 900.0, 1920.0, 2100.0]]    (523, -601, 1116, -62)
@@ -72,30 +71,43 @@ right [[0.0, 0.0, 1440.0, 900.0], [-1920.0, -300.0, 0.0, 900.0]])  (-1397, 599, 
 left  [[0.0, 0.0, 1440.0, 900.0], [1440.0, -300.0, 3360.0, 900.0]] (1963, 599, 2556, 1138)
 
 """
-    
+
 def test_frame_to_bounds():
     screens = get_screens()
     fbounds = frame_to_bounds(screens[0].frame())
-    assert type(fbounds).__name__ == 'tuple', 'frame bounds are nout a tuple'
+    assert type(fbounds).__name__ == 'list', 'frame bounds are nout a list'
     assert len(fbounds) == 4, 'frame bounds not four values'
     assert fbounds[0] < fbounds[2], 'left is greater than right'
     assert fbounds[1] < fbounds[3], 'top is greater than bottom'
 
-def test_get_windows_screen():
-    boundlist = (577, -562, 1170, -23), (0, 0, 100, 100)
-    for bounds in boundlist:
-        screen = get_windows_screen(bounds)
-        assert type(screen).__name__ == 'NSScreen', 'screen in not NSScreen'
-        frame = screen.frame()
-        assert type(frame).__name__ == 'NSRect', 'frame() does not return NSRect'
-        print('frame', frame)
-
 def test_which_screen_contains_window():
+    s0_win = (412, 23, 1013, 551)
+    print('Test with zero screen below')
+    s0_below = coordinate_screens(screen_zero_below())
+    s1_win = (461, -639, 1062, -111)
+    sid = which_screen_contains_window(s0_below, s0_win)
+    assert sid == 0, 'window should be on screen 0'
+    sid = which_screen_contains_window(s0_below, s1_win)
+    assert sid == 1, 'window should be on screen 1'
+    print('Test with zero screen above')
     s0_above = screen_zero_above()
-    s0_win = (0, 23, 100, 100)
     s1_win = (423, -618, 1024, -90)
     sid = which_screen_contains_window(s0_above, s0_win)
     assert sid == 0, 'window should be on screen 0'
     sid = which_screen_contains_window(s0_above, s1_win)
+    assert sid == 1, 'window should be on screen 1'
+    print('Test with zero screen right')
+    s0_right = screen_zero_right()
+    s1_win = (-1554, 481, -953, 1094)
+    sid = which_screen_contains_window(s0_right, s0_win)
+    assert sid == 0, 'window should be on screen 0'
+    sid = which_screen_contains_window(s0_right, s1_win)
+    assert sid == 1, 'window should be on screen 1'
+    print('Test with zero screen left')
+    s0_left = screen_zero_left()
+    s1_win = (1734, 207, 2335, 820)
+    sid = which_screen_contains_window(s0_left, s0_win)
+    assert sid == 0, 'window should be on screen 0'
+    sid = which_screen_contains_window(s0_left, s1_win)
     assert sid == 1, 'window should be on screen 1'
 
